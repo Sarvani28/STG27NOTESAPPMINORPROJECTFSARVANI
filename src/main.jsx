ee5ce0a1e3df630783e7eff179bd844d46b1fbff
Sarvani28 <saisarvanichaganti@gmail.com>
2025-07-20 11:08:53 +0530

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import "./index.css"
 import { Provider } from "react-redux"
 import { persistor, store } from "./redux/store.js"
 import { PersistGate } from "redux-persist/integration/react"
-
+import axios from "axios"
+axios.defaults.withCredentials = true
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
