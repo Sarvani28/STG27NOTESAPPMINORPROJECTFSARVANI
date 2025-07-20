ae52c383ee70650805fef2da01231bc350bd610d
Sarvani28 <saisarvanichaganti@gmail.com>
2025-07-20 11:26:01 +0530

    Updated NavBar files

--- src/components/Cards/ProfileInfo.jsx
+++ src/components/Cards/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { getInitials } from "../../utils/helper"
 
-const ProfileInfo = ({ onLogout, userInfo }) => {
+const ProfileInfo = ({  userInfo ,onLogout,}) => {
   return (
     <div className="flex items-center gap-3">
       <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
